cbcf689c86c1f51c6b199df71bacf244da69436f
Thor Jarle Kinstad <tj@cloudlink.ai>
2022-01-25 06:43:31 +0100

    added pandas series and dataframe

--- jupyter/notebooks/pandas/import_test.py
+++ jupyter/notebooks/pandas/import_test.py
@@ -1,9 +1,23 @@
 import numpy as np
 import pandas as pd
 
-data = 2,3,4,5,6,7
-index = 10,11,12, 16, 17 ,18
+# data = 2,3,4
+index = 10,11,12
 
-s = pd.Series(data, index=index)
+# test = pd.Series(data, index=index)
 
-print(s)
+# print(test)
+
+# A series (one-dimensional array) can hold any value
+
+val1= 1,2,3
+val2 =[4,5,6]
+val3 = "Nope"
+val4 = {"key1":"value1","key2":"value2"}
+
+s1 = pd.Series(val1, index=index)
+s2 = pd.Series(val2)
+s3 = pd.Series(val3)
+s4 = pd.Series(val4)
+
+print(s1,s2,s3,s4)
