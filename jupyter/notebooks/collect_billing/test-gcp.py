284634fd81c78165a3079c916b50b0ca7d701d49
cltj <tj@cloudlink.ai>
2022-08-16 20:01:34 +0200

    gcp query working

--- jupyter/notebooks/collect_billing/test-gcp.py
+++ jupyter/notebooks/collect_billing/test-gcp.py
@@ -1,31 +1,17 @@
-from config import My_Config as cfg
-import requests
+from google.cloud import bigquery
 import os
-from google.oauth2 import id_token
-from google.auth.transport.requests import Request
 
+credential_path = "/mnt/c/dev/cl/fun_with/jupyter/notebooks/collect_billing/cert/gcp-prices.privkey.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
-def auth_gcp():
-    credential_path = "/mnt/c/dev/cl/fun_with/jupyter/notebooks/collect_billing/cert/gcp-prices-358310-5df8416c6aa3.json"
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
-    token = id_token.fetch_id_token(Request(), cfg.gcp_client_id())
-    return token
+project_id='gcp-prices-358310'
+dataset_location='europe-west4'
+client = bigquery.Client(project=project_id, location=dataset_location)
 
-def get_gcp_prices():
-    token = auth_gcp()
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Accept": "application/json"
-    }
-    url = "https://cloudbilling.googleapis.com/v1/services"
-    response = requests.get(url, headers=headers)
-    return response
+query = """SELECT * FROM `gcp-prices-358310.12345.gcp_billing_export_v1_017DA2_854255_CCEB23`"""
 
+query_job = client.query(query)
+rows = query_job.result()  # Waits for job to complete.
+for row in rows:
+    print(row)
 
-def main():
-    response = get_gcp_prices()
-    print(response.text)
-
-
-if __name__ == "__main__":
-    main()
