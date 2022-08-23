8933ea8770729f3801e87c1fc1e4638e00eccd77
cltj <tj@cloudlink.ai>
2022-08-23 21:11:04 +0200

    experimented with zip

--- jupyter/notebooks/pydata-book-2nd-edition/testing/get_cols.py
+++ jupyter/notebooks/pydata-book-2nd-edition/testing/get_cols.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('costmanagementexport.csv')
 
@@ -6,11 +9,18 @@ cols = list(df.columns)
 col_len, row_len = df.shape
 
 #print("Columns: " + (str(col_len) + '\n' + "Rows: " + str(row_len)))
-
+columns = []
+count = []
 for elem in cols[0:65]:
     obj = pd.Series(df[elem].drop_duplicates())
-    if len(obj) > 1:
-        print(elem + ': ' + str(len(obj)), tuple(obj.values))
-    else:
-        print('Only one value')
+    print(elem, str(len(obj))) #, tuple(obj.values))
+    columns.append(elem)
+    count.append(str(len(obj)))
+
+my_dict = dict(zip(columns, count))
+df = pd.DataFrame(my_dict, index=[range(col_len)])
+print(df)
+
+
+
 
