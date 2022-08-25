11e209c14d5b88be58010b696f0152a8ae044d49
cltj <tj@cloudlink.ai>
2022-08-25 19:38:44 +0200

    google big query

--- jupyter/notebooks/collect_billing/test-gcp-billing-api.py
+++ jupyter/notebooks/collect_billing/test-gcp-billing-api.py
@@ -32,4 +32,6 @@ df.to_parquet('gcp/GCP-Billing-Data.parquet', index=False)
 #df2 = pd.read_csv('GCP-Billing-Data.csv')
 #print(df2.head())
 #len_row, len_col = df2.shape
+
+# What is diff in price between gbq and their storage solutions??
     
