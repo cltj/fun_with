863c0a4689a020f6e5dabb401c77bfb62dfd608b
cltj <tj@cloudlink.ai>
2022-09-04 06:17:27 +0200

    fixed SQL

--- jupyter/notebooks/collect_billing/test-gcp-billing-api.py
+++ jupyter/notebooks/collect_billing/test-gcp-billing-api.py
@@ -1,4 +1,5 @@
 from google.cloud import bigquery
+from config import My_Config as cfg
 import os
 import numpy as np
 import pandas as pd
@@ -10,10 +11,11 @@ credential_path = "/mnt/c/dev/cl/fun_with/jupyter/notebooks/collect_billing/cert
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 project_id='gcp-prices-358310'
-dataset_location='europe-west4'
+dataset_location='EU' # 'europe-west4'
+
 client = bigquery.Client(project=project_id, location=dataset_location)
 
-query = """SELECT * FROM `gcp-prices-358310.12345.gcp_billing_export_v1_017DA2_854255_CCEB23`"""
+query = """SELECT * FROM `gcp-prices-358310.billing.gcp_billing_export_resource_v1_017DA2_854255_CCEB23`"""
 
 s = pandas_gbq.read_gbq(query, project_id=project_id, dialect='standard')
 
@@ -21,13 +23,14 @@ df = pd.DataFrame(s, index=s.columns)
 len_row, len_col = df.shape
 
 print(df.head())
+print(df.columns)
 
 #Make a directory if it doesn't exist
 if not os.path.exists('gcp'):
     os.makedirs('gcp')
 
-#df.to_csv('gcp/GCP-Billing-Data.csv', index=False)
-df.to_parquet('gcp/GCP-Billing-Data.parquet', index=False)
+#df.to_csv('gcp/3GCP-Billing-Data.csv', index=False)
+df.to_parquet('gcp/3GCP-Billing-Data.parquet', index=False)
 
 #df2 = pd.read_csv('GCP-Billing-Data.csv')
 #print(df2.head())
