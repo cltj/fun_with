2af127fa2d59bafe865ef1e61f957367d002d01e
cltj <tj@cloudlink.ai>
2022-09-28 19:29:40 +0200

    added assert

--- jupyter/notebooks/simulate_pipeline/main.py
+++ jupyter/notebooks/simulate_pipeline/main.py
@@ -1,6 +1,8 @@
+from ast import Assert
 from config import My_Config as cfg
 from extract import Get_billing
 from combine import Combine
+from asrt import Assert
 
 
 def extract():
@@ -10,16 +12,24 @@ def extract():
     Get_billing.gcp_billing()
     print("Extraction complete")
 
+
 def combine():
     print("Deviding data")
     table = cfg.storage_blob_name()
     Combine.main(table)
     print("Data combined")
 
+
+def asrt():
+    print("Checking assertion")
+    Assert.main()
+    print("Assertion made")
+
+
 def main():
     extract()
     combine()
-    
+    asrt()
 
 
 if __name__ == '__main__':
