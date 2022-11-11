fb110d17de668b8cf9c1d380044c288fa947089a
cltj <tj@cloudlink.ai>
2022-11-11 23:50:41 +0100

    dataclasses

---
+++ jupyter/notebooks/demos/test_dataclass.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+@dataclass
+class GfgArticle():
+    """A class for holding an article content"""
+ 
+    # Attributes Declaration
+    # using Type Hints
+ 
+    title: str
+    author: str
+    language: str
+    upvotes: int
+ 
+# A DataClass object
+article = GfgArticle("DataClasses",
+                     "vibhu4agarwal",
+                     "Python", 0)
+print(article)
