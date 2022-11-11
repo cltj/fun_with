1ccdf28b8704e8cebf472d3427a7386c6307feb3
cltj <tj@cloudlink.ai>
2022-11-11 23:53:02 +0100

    added dataclass

--- jupyter/notebooks/demos/test_dataclass.py
+++ jupyter/notebooks/demos/test_dataclass.py
@@ -17,3 +17,33 @@ article = GfgArticle("DataClasses",
                      "vibhu4agarwal",
                      "Python", 0)
 print(article)
+
+
+
+##
+
+class NormalArticle():
+    """A class for holding an article content"""
+ 
+    # Equivalent Constructor
+    def __init__(self, title, author, language, upvotes):
+        self.title = title
+        self.author = author
+        self.language = language
+        self.upvotes = upvotes
+ 
+# Two DataClass objects
+dClassArticle1 = GfgArticle("DataClasses",
+                            "vibhu4agarwal",
+                            "Python", 0)
+dClassArticle2 = GfgArticle("DataClasses",
+                            "vibhu4agarwal",
+                            "Python", 0)
+ 
+# Two objects of a normal class
+article1 = NormalArticle("DataClasses",
+                         "vibhu4agarwal",
+                         "Python", 0)
+article2 = NormalArticle("DataClasses",
+                         "vibhu4agarwal",
+                         "Python", 0)
